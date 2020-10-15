d2aeefff89e6d466fc2f399cada3c243ad5e047b
hamsoace <hamsoace@gmail.com>
2020-10-15 14:24:10 +0300

    remove in excess code

--- src/LearnJava.java
+++ src/LearnJava.java
@@ -102,35 +102,8 @@ public class LearnJava {
             //3. System Testing--Test the technical, function and business in a code
             //4. Acceptance Testing--User Acceptance Testing - for verification, performance requirements
 
-        public static void encrypt(String messageText, int changeChar){
-            String letterBig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            String letterSmall = letterBig.toLowerCase();
+      
 
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter You Text:  ");
-            String message = scan.next();
-            int length = message.length();
-            System.out.println("Enter the key: ");
-            int key = scan.nextInt();
-
-            for(int i = 0; i< length; i++){
-                for (int j = 0; j< 26; j++){
-                    if (message.charAt(i) == letterSmall.charAt(j)){
-                        System.out.println(letterSmall.charAt((j + key) % 26));
-                    }else if(message.charAt(i) == letterBig.charAt(j)){
-                        System.out.println(letterBig.charAt((j + key) % 26));
-                    }
-
-                }
-
-            }
-
-
-        }
-        public static void main(String[] args){
-            System.out.println("The encrypted message is:  ");
-
-        }
 
     }
 
